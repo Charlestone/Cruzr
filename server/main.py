f99a791d9e96f4a43ec55e0a9544eda5ff5a4b81
Lewis Bell <lewisbell999@gmail.com>
2019-01-19 18:42:08 -0800

    mv server to main

--- server/main.py
+++ server/main.py
@@ -172,5 +172,8 @@ def sendEmail(cruzerMail, cruzedMail, tag) :
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message.as_string())
 
-
-
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
